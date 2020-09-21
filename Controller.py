3bb9af7292d3cff400f95dfa9f40974cc05dba2a
Christo-IV <christo.vahtramae@khk.ee>
2020-09-21 12:31:52 +0300

    Added/Updated element update/deletion/addition methods

--- Controller.py
+++ Controller.py
@@ -1,12 +1,7 @@
-class EiLeitudElementi(Exception):
-    pass
-
-
 class Controller:
     def __init__(self, p_model, p_view):
         self.model = p_model
         self.view = p_view
-
 
     # Elemendi kuvamine
     def kuva_elemendid(self):
@@ -14,8 +9,34 @@ class Controller:
         self.view.kuva_elemendid(elemendid)
 
     def kuva_element(self, p_nimetus):
-        try:
-            element = self.model.loe_element(p_nimetus)
-        except EiLeitudElementi:
-            element = {}
-        self.view.kuva_element(element)
+        element, veateade = self.model.loe_element(p_nimetus)
+        if veateade == "":
+            self.view.kuva_element(element)
+        else:
+            self.view.veateade(veateade)
+
+    def lisa_element(self, p_nimetus, p_hind, p_kogus):
+        if p_hind <= 0:
+            self.view.veateade("Hind peab olema suurem kui 0")
+        elif p_kogus <= 0:
+            self.view.veateade("Kogus peab olema suurem kui 0")
+        else:
+            self.model.lisa_element(p_nimetus, p_hind, p_kogus)
+            self.view.lisa_element(p_nimetus, p_hind, p_kogus)
+
+    def uuenda_elementi(self, p_nimetus, p_hind, p_kogus):
+        if p_hind <= 0:
+            self.view.veateade("Hind peab olema suurem kui 0")
+        elif p_kogus <= 0:
+            self.view.veateade("Kogus peab olema suurem kui 0")
+        else:
+            vana_hind, vana_kogus = self.model.uuenda_elementi(p_nimetus, p_hind, p_kogus)
+            self.view.uuenda_elementi(p_nimetus, vana_hind, p_hind, vana_kogus, p_kogus)
+
+    def kustuta_element(self, p_nimetus):
+        self.model.kustuta_element(p_nimetus)
+        self.view.kustuta_element(p_nimetus)
+
+    def kustuta_kõik(self):
+        self.model.kustuta_kõik()
+        self.view.kustuta_kõik()
